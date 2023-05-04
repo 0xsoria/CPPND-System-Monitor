6509aef8c1c025acf492519d3206ca7881fdb10a
Gabriel Sória <gabrielss.rock@me.com>
2023-05-04 21:08:14 +0100

    Updated format after code review

--- src/format.cpp
+++ src/format.cpp
@@ -10,5 +10,14 @@ string Format::ElapsedTime(long seconds) {
     seconds = seconds % 3600;
     minutes = seconds / 60;
     seconds = seconds % 60;
-    return std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds);
+    
+    string sec = std::to_string(seconds);
+    sec.insert(0, 2 - sec.length(), '0');
+    
+    string mins = std::to_string(minutes);
+    mins.insert(0, 2 - mins.length(), '0');
+    
+    string hrs = std::to_string(hours);
+    hrs.insert(0, 2 - hrs.length(), '0');
+    return hrs + ":" + mins + ":" + sec;
 }
